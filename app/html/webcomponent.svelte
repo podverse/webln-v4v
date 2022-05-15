2927c07a872be0eccc8042095d80257703a7c811
Mitch Downey <mitchldowney@gmail.com>
2022-05-15 00:27:02 -0500

    Add normalizedSplits and onChange handling

--- app/html/webcomponent.svelte
+++ app/html/webcomponent.svelte
@@ -13,6 +13,35 @@
 	// import webcomponent from "@app/functions/webcomponent";
 	// import { translate } from "@app/translations/translate";
 
+	type ValueTag = {
+		method: string;
+		suggested: string;
+		type: string;
+		recipients: ValueRecipient[];
+	};
+
+	type ValueRecipient = {
+		address: string;
+		customKey?: string;
+		customValue?: any;
+		fee?: boolean | null;
+		name?: string;
+		split: number | string;
+		type: string;
+	};
+
+	type ValueRecipientNormalized = {
+		address: string;
+		amount: number;
+		customKey?: string;
+		customValue?: any;
+		fee?: boolean | null;
+		name?: string;
+		normalizedSplit: number;
+		split: number | string;
+		type: string;
+	};
+
 	export let amount_max: string;
 	export let amount_min: string;
 	export let app_name: string;
@@ -32,13 +61,13 @@
 	let amountMin = amount_min || 0;
 	let amountMax = amount_max || 500000;
 	let appName = app_name || "Unknown App";
-	let appRecipientLNAdress = app_recipient_ln_address || "";
+	let appRecipientLNAddress = app_recipient_ln_address || "";
 	let appRecipientLabel = app_recipient_label || "App";
 	let appRecipientValue = app_recipient_value_default || 0;
 	let headerText = header_text || "Send a Bitcoin donation to this content creator and app.";
 	let messageLabel = message_label || "Boostagram";
 	let messagePlaceholder = message_placeholder || "(optional message)";
-	let messageValue = "";
+	let message = "";
 	let name2 = name || "Anonymous"; // not sure what "name" is for in Alby
 	let recipientLabel = recipient_label || "Content Creator";
 	let recipientValue = recipient_value_default || 0;
@@ -52,16 +81,26 @@
 	let lnpayTermsRejected = false;
 	let termsAcceptCheckboxValue = false;
 
+	let boostPromises = [];
+	let normalizedRecipients = [];
+
+	$: handleAppRecipientAmountOnChange(appRecipientValue);
+	$: handleRecipientAmountOnChange(recipientValue);
+
 	const initialize = () => {
 		// check if the user has webln and keysend (currently Alby)
-		return !(typeof window.webln === "undefined" || !window.webln.keysend);
+		// return !(typeof window.webln === "undefined" || !window.webln.keysend);
+
+		// TODO: add initialize check
+		return true;
 	};
 
 	const enableWebLN = async () => {
 		if (!errorMessage) {
 			try {
 				// enable webln / request website permission
-				await window.webln.enable();
+				// TODO: disabling this for dev purposes for now
+				// await webln.enable();
 				lnpayEnabled = true;
 			} catch (error) {
 				errorMessage = "User denied permission or cancelled.";
@@ -82,7 +121,7 @@
 	const sendBoost = () => {
 		console.log("do validation", appRecipientValue);
 		console.log("get values", recipientValue);
-		console.log("send boost", messageValue);
+		console.log("send boost", message);
 	};
 
 	// const validateBoost = () => {
@@ -90,36 +129,146 @@
 	// 	// }
 	// };
 
-	const parseV4VStringIntoBoostObjects = (v4vString: string) => {
-		try {
-			const v4vObject = JSON.parse(v4vString);
+	const calculateNormalizedSplits = (recipients: ValueRecipient[]) => {
+		let normalizedValueRecipients: ValueRecipientNormalized[] = [];
 
-			if (
-				v4vObject.type === "lightning" &&
-				v4vObject.method === "keysend" &&
-				v4vObject.recipients &&
-				v4vObject.recipients.length > 0
-			) {
+		const totalSplit = recipients.reduce((total, recipient) => {
+			return total + parseFloat(recipient.split);
+		}, 0);
+
+		normalizedValueRecipients = recipients.map((recipient) => {
+			return {
+				...recipient,
+				normalizedSplit: (parseFloat(recipient.split) / totalSplit) * 100,
+				amount: 0, // TODO: hacky code. This will get set in normalizeValueRecipients.
+			};
+		});
+
+		normalizedValueRecipients = normalizedValueRecipients.filter((x) => isValidNormalizedValueRecipient(x));
+
+		return normalizedValueRecipients;
+	};
+
+	const isValidNormalizedValueRecipient = (normalizedValueRecipient: ValueRecipientNormalized) =>
+		!!(
+			normalizedValueRecipient?.address &&
+			normalizedValueRecipient?.amount > 0 &&
+			normalizedValueRecipient?.normalizedSplit > 0 &&
+			normalizedValueRecipient?.split > 0 &&
+			normalizedValueRecipient?.type
+		);
+
+	export const normalizeValueRecipients = (recipients: ValueRecipient[], total: number, roundDownValues: boolean) => {
+		const normalizedValueRecipients: ValueRecipientNormalized[] = calculateNormalizedSplits(recipients);
+		const feeRecipient = normalizedValueRecipients.find((valueRecipient) => valueRecipient.fee === true);
+		let feeAmount = 0;
+		if (feeRecipient) {
+			feeAmount = (total / 100) * (feeRecipient.normalizedSplit || 0);
+			total = total - feeAmount;
+		}
+
+		const finalNormalizedValueRecipients: ValueRecipientNormalized[] = [];
+		for (const normalizedValueRecipient of normalizedValueRecipients) {
+			let amount = (total / 100) * (normalizedValueRecipient.normalizedSplit || 0);
+
+			if (feeAmount && normalizedValueRecipient.fee) {
+				amount = feeAmount;
+			}
+
+			amount = roundDownValues ? Math.floor(amount) : amount;
+
+			finalNormalizedValueRecipients.push({
+				...normalizedValueRecipient,
+				amount: parseFloat(amount.toFixed(2)),
+			});
+		}
+
+		return finalNormalizedValueRecipients;
+	};
+
+	const prepareBoostPromises = (v4v: any, newRecipientValue: number, newAppRecipientValue: number) => {
+		try {
+			boostPromises = [];
+			recipientValue = newRecipientValue;
+			appRecipientValue = newAppRecipientValue;
+			const { recipients } = v4v;
+			const roundDownValues = true;
+			normalizedRecipients = normalizeValueRecipients(recipients, recipientValue, roundDownValues);
+
+			if (v4v.type === "lightning" && v4v.method === "keysend" && v4v.recipients && v4v.recipients.length > 0) {
+				for (const recipient of normalizedRecipients) {
+					const boost = {
+						action: "boost",
+						value_msat_total: recipient.amount * 1000,
+						app_name,
+						sender_name,
+						name: "satoshi",
+						message,
+					};
+
+					const { address, amount } = recipient;
+					if (address && amount >= 10) {
+						boostPromises.push(
+							webln.keysend({
+								destination: address,
+								amount,
+								customRecords: {
+									"7629169": JSON.stringify(boost),
+								},
+							}),
+						);
+					}
+				}
+
+				if (appRecipientLNAddress && appRecipientValue >= 10) {
+					const boost = {
+						action: "boost",
+						value_msat_total: appRecipientValue,
+						app_name,
+						sender_name,
+						name,
+						message,
+					};
+
+					boostPromises.push(
+						webln.keysend({
+							destination: appRecipientLNAddress,
+							amount: appRecipientValue,
+							customRecords: {
+								"7629169": JSON.stringify(boost),
+							},
+						}),
+					);
+				}
+			} else {
 				errorMessage = "Invalid v4v data.";
 				throw new Error(errorMessage);
 			}
-
-			return ["asdf"];
 		} catch (error: any) {
 			errorMessage = error.message;
 			return [];
 		}
 	};
 
-	let boostObjects = [];
+	const handleRecipientAmountOnChange = (val: number) => {
+		const v4vObject = JSON.parse(v4v_tag);
+		boostPromises = prepareBoostPromises(v4vObject, recipientValue, appRecipientValue);
+	};
+
+	const handleAppRecipientAmountOnChange = (val: number) => {
+		const v4vObject = JSON.parse(v4v_tag);
+		boostPromises = prepareBoostPromises(v4vObject, recipientValue, appRecipientValue);
+	};
+
 	lnpayInitialized = initialize();
-	console.log("lnpayInitialized", lnpayInitialized);
+
 	if (lnpayInitialized && v4v_tag) {
-		boostObjects = parseV4VStringIntoBoostObjects(v4v_tag);
+		const v4vObject = JSON.parse(v4v_tag);
+		boostPromises = prepareBoostPromises(v4vObject, recipientValue, appRecipientValue);
 	}
 </script>
 
-{#if lnpayInitialized && boostObjects.length > 0 && !lnpayTermsRejected}
+{#if lnpayInitialized && !lnpayTermsRejected}
 	<div id="webcomponent" part="webcomponent">
 		{#if errorMessage}
 			{`ERROR: ${errorMessage}`}
@@ -160,6 +309,7 @@
 							id="content-creator-amount"
 							max={amountMax}
 							min={amountMin}
+							on:input={handleRecipientAmountOnChange}
 							type="number"
 							bind:value={recipientValue}
 						/>
@@ -173,6 +323,7 @@
 							id="app-amount"
 							max={amountMax}
 							min={amountMin}
+							on:input={handleAppRecipientAmountOnChange}
 							type="number"
 							bind:value={appRecipientValue}
 						/>
@@ -181,7 +332,7 @@
 				</div>
 				<div class="input-wrapper">
 					<label for="boostagram">{messageLabel}</label>
-					<textarea id="boostagram" placeholder={messagePlaceholder} bind:value={messageValue} />
+					<textarea id="boostagram" placeholder={messagePlaceholder} bind:value={message} />
 				</div>
 				<div class="buttons-wrapper">
 					<button type="submit">{sendButtonLabel}</button>
